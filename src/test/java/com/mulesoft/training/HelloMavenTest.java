547d88ba91260c1373478f458a1a2aea61f4c5b8
cermu <kwendosamson750@gmail.com>
2019-01-17 11:34:42 +0300

    changed mule-mode-json version to 3.8.1

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,7 +1,10 @@
 package com.mulesoft.training;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.api.MuleEvent;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
@@ -13,6 +16,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	MuleEvent event = runFlow("retrieveFlights");
+    	String contentType = event.getMessage().getOutboundProperty("Content-Type");
+    	assertEquals("application/json", contentType);
     }
 
 }
